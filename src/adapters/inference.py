c245da7c27c14ab7242e256a225f09091e5ac6c9
Noel <jwnwilson@hotmail.co.uk>
2026-06-01 09:22:38 +0100

    Enable memory locking for model on Raspberry Pi
    Changed use_mlock to True to pin model in RAM and prevent swapping on Raspberry Pi.

--- src/adapters/inference.py
+++ src/adapters/inference.py
@@ -93,7 +93,7 @@ class LlamaCppInferenceAdapter(InferencePort):
             n_ctx=self._context_size,
             n_threads=n_threads,
             n_batch=512,
-            use_mlock=False,
+            use_mlock=True,   # pin model in RAM, prevent swap on RPi
             verbose=True,
         )
 
