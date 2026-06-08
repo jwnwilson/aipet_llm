d33b3ca6ed145cd2dd1d67d4b3477756de90818c
Noel Wilson <noel@MacBookPro-3.powerhub>
2026-06-08 23:46:40 +0100

    fix: add PATCH to CORS allowed methods

--- src/interactors/api/app.py
+++ src/interactors/api/app.py
@@ -193,7 +193,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=_cors_origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
     allow_headers=["Authorization", "Content-Type"],
 )
 
